ca955700d2a55db34bc9a75093053fe042d878fb
HigheEloPlayer <nfcobra20@gmail.com>
2023-04-28 10:51:36 +0800

    First Commit

---
+++ PDC03_MODULE04/PDC03_MODULE04/activity01.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity01 : ContentPage
+    {
+        public activity01()
+        {
+            InitializeComponent();
+        }
+        private async void OpenActivity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity01());
+        }
+
+        private async void OpenActivity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity02());
+        }
+    }
+}
